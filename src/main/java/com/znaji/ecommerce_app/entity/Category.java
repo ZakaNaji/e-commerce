cca4fd01a02904af16d79b06cd54b8d0d8b1f3b1
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-19 19:00:11 +0100

    #Category: add Lombok and use it, to reduce boilerplate code

--- src/main/java/com/znaji/ecommerce_app/entity/Category.java
+++ src/main/java/com/znaji/ecommerce_app/entity/Category.java
@@ -1,36 +1,18 @@
 package com.znaji.ecommerce_app.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_generator")
     @SequenceGenerator(name = "category_id_generator", sequenceName = "category_id_seq", allocationSize = 1)
     private Long categoryId;
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
