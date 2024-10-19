661a7e60cf74e477167432963764004a99cf358e
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-19 18:29:04 +0100

    #Category: make our entity a jpa managed entity.

--- src/main/java/com/znaji/ecommerce_app/entity/Category.java
+++ src/main/java/com/znaji/ecommerce_app/entity/Category.java
@@ -1,12 +1,21 @@
 package com.znaji.ecommerce_app.entity;
 
+import jakarta.persistence.*;
+
+@Entity(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_generator")
+    @SequenceGenerator(name = "category_id_generator", sequenceName = "category_id_seq", allocationSize = 1)
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
     }
 
     public Long getCategoryId() {
