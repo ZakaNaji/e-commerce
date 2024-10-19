6f7968f3f24ad7a546c2c3692b4d8a9ea95aeec1
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-19 18:30:19 +0100

    #Category: create a jpa repository for our category entity. and use it in our service instead of local list.

--- src/main/java/com/znaji/ecommerce_app/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/znaji/ecommerce_app/service/impl/CategoryServiceImpl.java
@@ -1,24 +1,28 @@
 package com.znaji.ecommerce_app.service.impl;
 
 import com.znaji.ecommerce_app.entity.Category;
+import com.znaji.ecommerce_app.repository.CategoryRepository;
 import com.znaji.ecommerce_app.service.CategoryService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final List<Category> categories = new ArrayList<>();
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @Override
     public Category createCategory(Category category) {
-        categories.add(category);
-        return category;
+        return categoryRepository.save(category);
     }
 
     @Override
     public List<Category> findAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 }
