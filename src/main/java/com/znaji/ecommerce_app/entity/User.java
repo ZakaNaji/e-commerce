1e393fded6a2ce563a85622ae11092e30f4543e4
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-28 23:34:15 +0100

    #User: create User entity and add ManyToMany mapping config(will be using a join table to map the relationship)

---
+++ src/main/java/com/znaji/ecommerce_app/entity/User.java
@@ -0,0 +1,35 @@
+package com.znaji.ecommerce_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "username")
+    private String username;
+    private String password;
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    List<Role> roles;
+}
