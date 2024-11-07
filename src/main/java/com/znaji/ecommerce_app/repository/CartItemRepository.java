dbb10a0f0cbe7c85d878eac434785519ecdcc9fa
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-07 19:50:09 +0100

    #User: CartItemRepository

---
+++ src/main/java/com/znaji/ecommerce_app/repository/CartItemRepository.java
@@ -0,0 +1,13 @@
+package com.znaji.ecommerce_app.repository;
+
+import com.znaji.ecommerce_app.entity.Cart;
+import com.znaji.ecommerce_app.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCart(Cart cart);
+}
