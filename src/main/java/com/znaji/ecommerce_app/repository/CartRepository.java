0a299717a83f403dbf1d0494b42f0bd67699e349
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-11-09 14:18:41 +0100

    #User: CartRepository

--- src/main/java/com/znaji/ecommerce_app/repository/CartRepository.java
+++ src/main/java/com/znaji/ecommerce_app/repository/CartRepository.java
@@ -3,9 +3,14 @@ package com.znaji.ecommerce_app.repository;
 import com.znaji.ecommerce_app.entity.Cart;
 import com.znaji.ecommerce_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUser(User user);
+
+    @Query("SELECT c FROM Cart c WHERE c.user.username = ?1")
+    Optional<Cart> findByUsername(String username);
 }
